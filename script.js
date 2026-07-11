e7a9212bac3d5ce3fd6979118bc8b0289a6f7586
anjalijaiswal4661 <161955948+anjalijaiswal4661@users.noreply.github.com>
2026-07-11 18:08:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,25 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
+	let totalCost = 0
+	while(arr.length > 1){
+		arr.sort((a,b) => a-b);
+		let first = arr.shift();
+		let second = arr.shift();
+
+		let cost = first+second;
+		totalCost+=cost;
+
+		arr.push(cost)
+	}
+	return totalCost;
+	 
   
 }
-
+let input=prompt ("Enter the number of element")
+let arr=[]
+for(let i=0;i<input;i++){
+	arr=arr.push(prompt(`Enter the ${i+1} element`))
+}
+alert(mincost(arr))
 module.exports=mincost;
